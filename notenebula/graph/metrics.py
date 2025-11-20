c9c0569e011c68d88b1fbcf84136381379239a17
JessicaSyb <karanbudha072@gmail.com>
2025-11-20 19:57:27 +0000

    Polish notenebula/graph/metrics.py a little bit

--- notenebula/graph/metrics.py
+++ notenebula/graph/metrics.py
@@ -12,3 +12,5 @@ def in_degree(g: Dict[str, List[str]]):
 def top_by_out(g: Dict[str, List[str]], n:int=5) -> List[Tuple[str,int]]:
     return sorted(((k,len(v)) for k,v in g.items()), key=lambda x:-x[1])[:n]
 # tweak 2025-11-17T20:40:32.090587+00:00
+
+# autosave 2025-11-20T19:57:27.073665+00:00
