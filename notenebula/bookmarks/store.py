983d59c60afec6fec05d16ed40c482f546ba7129
JessicaSyb <karanbudha072@gmail.com>
2025-11-24 20:15:53 +0000

    Keep notenebula/bookmarks/store.py tdiy (no logic changes)

--- notenebula/bookmarks/store.py
+++ notenebula/bookmarks/store.py
@@ -19,3 +19,5 @@ def add(url: str, title: Optional[str]=None, tags: Optional[List[str]]=None, not
 
 # autosave 2025-10-20T12:39:42.979463+00:00
 # tweak 2025-11-03T19:44:23.178572+00:00
+
+# autosave 2025-11-24T20:15:53.156889+00:00
