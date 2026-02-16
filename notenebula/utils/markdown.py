98a158ce440239e24f646f8090f5c1e907854d9c
JessicaSyb <karanbudha072@gmail.com>
2026-02-16 13:42:24 +0000

    Quality-of-life tweaks

--- notenebula/utils/markdown.py
+++ notenebula/utils/markdown.py
@@ -9,3 +9,4 @@ def headings(md: str): return [(m.group(1), m.group(2).strip()) for m in HDR.fin
 # autosave 2025-12-08T20:46:18.393039+00:00
 
 # autosave 2026-01-26T16:27:25.355786+00:00
+# tweak 2026-02-16T13:42:23.996040+00:00
